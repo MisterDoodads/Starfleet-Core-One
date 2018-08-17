1aa1af52ab8657c1c2adfe876a7065bfd2e97c25
Gaston Ayoroa <Gaston.Ayoroa@ncci.com>
2018-08-17 14:00:22 -0400

    First Commit

---
+++ Hello world - VStudio/Hello World - VStudio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World___VStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome, {0}, to my world.", name);
+            Console.ReadLine();
+        }
+    }
+}
